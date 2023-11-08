7f2173f55baca0d17c35fa5622163d4fc4476320
davide-williams <DavidE.Williams@vita.virginia.gov>
2023-11-08 18:41:54 -0500

    updates

---
+++ helloworld-executable/src/main/java/gov/vita/ais/examples/helloworld/Main.java
@@ -0,0 +1,35 @@
+/**
+ *  Copyright 2005-2016 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package gov.vita.ais.examples.helloworld;
+
+import java.util.Date;  
+
+/**
+ * Example of the "main class". Put your bootstrap logic here.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws InterruptedException the interrupted exception
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Hello from the executable jar! The current time is: " +  new Date());
+	}
+
+}
---
+++ helloworld-executable/src/test/java/gov/vita/ais/examples/dateExampleJar/MainTest.java
@@ -0,0 +1,10 @@
+package gov.vita.ais.examples.dateExampleJar;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void fakeTest() {
+	}
+}
