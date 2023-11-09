b1a88968a26d16bd794d9b01a67516128b540498
davide-williams <DavidE.Williams@vita.virginia.gov>
2023-11-08 19:27:51 -0500

    updates

--- helloworld-executable/src/test/java/gov/vita/ais/examples/dateExampleJar/MainTest.java
+++ helloworld-executable/src/test/java/gov/vita/ais/examples/dateExampleJar/MainTest.java
@@ -1,10 +1,15 @@
 package gov.vita.ais.examples.dateExampleJar;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class MainTest {
 
 	@Test
 	public void fakeTest() {
+		 assertTrue(true);
+		 //assertFalse("The test failed", false);
 	}
 }
